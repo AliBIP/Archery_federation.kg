a3a5e26a725e06cfb3a06e0463e86194cfa06df7
Ali <ali@example.com>
2025-05-05 16:07:20 +0600

    Hello

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ class News(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     content = db.Column(db.Text, nullable=False)
+
     date = db.Column(db.DateTime, default=datetime.utcnow)
     category = db.Column(db.String(50))
     image_url = db.Column(db.String(200))
